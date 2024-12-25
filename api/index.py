13ff7292656d04e916cf6c49444384d8da4cd5d1
Andrew Wong <wongandrew97@gmail.com>
2024-12-25 18:54:29 -0500

    Redeploy as FastAPI

--- api/index.py
+++ api/index.py
@@ -1,20 +1,18 @@
-from http.server import BaseHTTPRequestHandler
+from .get_available import get_docks_available
 
-from get_available import get_docks_available
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        string_builder = []
-        open_docks = get_docks_available()
-        for i, (station_name, num_docks) in enumerate(open_docks.items()):
-            if i < len(open_docks)-1:
-                string_builder.append(f"{station_name} has {num_docks} open docks\n")
-            else:
-                string_builder.append(f"{station_name} has {num_docks} open docks")
+app = FastAPI()
 
-        formatted_response = ''.join(string_builder)
+@app.get("/")
+async def main():
+    string_builder = []
+    open_docks = get_docks_available()
+    for i, (station_name, num_docks) in enumerate(open_docks.items()):
+        if i < len(open_docks)-1:
+            string_builder.append(f"{station_name} has {num_docks} open docks\n")
+        else:
+            string_builder.append(f"{station_name} has {num_docks} open docks")
 
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(formatted_response.encode('utf-8'))
+    return PlainTextResponse(''.join(string_builder))
